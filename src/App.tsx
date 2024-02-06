f1d64a6211c39512264cf0c987d6fcd65f6a61c6
Hemant Chaudhary <hchaudhary1994@gmail.com>
2024-02-06 22:09:32 +0530

    base project created with react + typescript + tailwind + vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className='font-bold text-3xl bg-slate-300'>
+        Your Home
+      </p>
+    </>
+  )
+}
+
+export default App
